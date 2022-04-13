b48c80f4222a17a71347e908a990d3991e8515cf
Arthur Hanson <worldnomad@gmail.com>
2022-04-13 15:41:47 -0700

    update version for pep440

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -1,6 +1,6 @@
 from .types import DjangoObjectType
 from .fields import DjangoConnectionField
 
-__version__ = "2.5.0.pm.forked.4"
+__version__ = "2.5.0+pm.forked.4"
 
 __all__ = ["__version__", "DjangoObjectType", "DjangoConnectionField"]
